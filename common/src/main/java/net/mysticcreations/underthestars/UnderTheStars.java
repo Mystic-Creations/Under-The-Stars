93e066653442e1323b95da4d687d132ce16f1f16
JustMili <rodx160@gmail.com>
2026-01-18 16:35:31 +0100

    Rewritten HealingCampfire and put us on Parchment mappings

--- common/src/main/java/net/mysticcreations/underthestars/UnderTheStars.java
+++ common/src/main/java/net/mysticcreations/underthestars/UnderTheStars.java
@@ -1,9 +1,11 @@
 package net.mysticcreations.underthestars;
 
+import dev.architectury.event.events.common.TickEvent;
 import net.minecraft.resources.ResourceLocation;
 import net.mysticcreations.underthestars.init.UtsBlocks;
 import net.mysticcreations.underthestars.init.UtsItems;
 import net.mysticcreations.underthestars.init.UtsTabs;
+import net.mysticcreations.underthestars.mechanics.HealingCampfire;
 
 public final class UnderTheStars {
     public static final String MODID = "underthestars";
@@ -12,6 +14,11 @@ public final class UnderTheStars {
         UtsBlocks.register();
         UtsItems.register();
         UtsTabs.register();
+        registerEvents();
+    }
+
+    public static void registerEvents() {
+        TickEvent.PLAYER_POST.register(HealingCampfire::onPlayerTick);
     }
 
     public static ResourceLocation asResource(String path) {
--- common/src/main/java/net/mysticcreations/underthestars/mechanics/HealingCampfire.java
+++ common/src/main/java/net/mysticcreations/underthestars/mechanics/HealingCampfire.java
@@ -1,4 +1,46 @@
 package net.mysticcreations.underthestars.mechanics;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.CampfireBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+
 public class HealingCampfire {
+    public static void onPlayerTick(Player player) {
+        Level level = player.level();
+        BlockPos playerPos = player.blockPosition();
+
+        boolean nearCampfire = false;
+        AABB area = new AABB(
+            playerPos.offset(-6, -2, -6),
+            playerPos.offset(7, 3, 7)
+        );
+
+        for (BlockPos pos : BlockPos.betweenClosed(
+            (int) area.minX, (int) area.minY, (int) area.minZ,
+            (int) area.maxX - 1, (int) area.maxY - 1, (int) area.maxZ - 1
+        )) {
+            BlockState state = level.getBlockState(pos);
+
+            if (state.getBlock() instanceof CampfireBlock &&
+                state.getValue(CampfireBlock.LIT)) {
+                nearCampfire = true;
+                break;
+            }
+        }
+        if (nearCampfire) {
+            player.addEffect(new MobEffectInstance(
+                MobEffects.REGENERATION,
+                100,
+                0,
+                false,
+                false,
+                false
+            ));
+        }
+    }
 }
