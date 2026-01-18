055d6473c016e23b50c44df398809c3fdf548167
JustMili <rodx160@gmail.com>
2026-01-18 19:31:08 +0100

    Chocolate fully implemented, just needs new textures

--- common/src/main/java/net/mysticcreations/underthestars/item/ChocolateBar.java
+++ common/src/main/java/net/mysticcreations/underthestars/item/ChocolateBar.java
@@ -2,6 +2,13 @@
 package net.mysticcreations.underthestars.item;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Wolf;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -30,6 +37,26 @@ public class ChocolateBar extends Item {
 	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, world, list, flag);
 	}
+
+    @Override
+    public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity target, InteractionHand hand) {
+        Level level = player.level();
+
+        if (!(target instanceof Wolf wolf)) {
+            return InteractionResult.PASS;
+        }
+        if (!level.isClientSide) {
+            wolf.addEffect(new MobEffectInstance(
+                MobEffects.POISON,
+                150,
+                0
+            ));
+
+            stack.shrink(1);
+        }
+
+        return InteractionResult.sidedSuccess(level.isClientSide);
+    }
 }
 
 
