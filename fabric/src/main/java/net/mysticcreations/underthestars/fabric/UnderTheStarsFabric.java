4155f3d1fbf658fc80e23e9eeb11caaddf44abf9
eetgeenappels <eetgeenappels@gmail.com>
2026-01-28 00:45:27 +0100

    add proper terrablender integration also added template features so game no crash and also removed old_growth_oak_trees cause that does not exist so it crashed too yeah

--- fabric/src/main/java/net/mysticcreations/underthestars/fabric/UnderTheStarsFabric.java
+++ fabric/src/main/java/net/mysticcreations/underthestars/fabric/UnderTheStarsFabric.java
@@ -2,11 +2,20 @@ package net.mysticcreations.underthestars.fabric;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.resources.ResourceLocation;
 import net.mysticcreations.underthestars.UnderTheStars;
+import net.mysticcreations.underthestars.worldgen.UTSRegion;
+import terrablender.api.Regions;
+import terrablender.api.TerraBlenderApi;
 
-public final class UnderTheStarsFabric implements ModInitializer {
+public final class UnderTheStarsFabric implements ModInitializer, TerraBlenderApi {
     @Override
     public void onInitialize() {
         UnderTheStars.init();
+    }
+
+    @Override
+    public void onTerraBlenderInitialized() {
+        Regions.register(new UTSRegion(new ResourceLocation("under_the_stars:overworld_region"), 1));
     }
 }
--- forge/src/main/java/net/mysticcreations/underthestars/forge/UnderTheStarsForge.java
+++ forge/src/main/java/net/mysticcreations/underthestars/forge/UnderTheStarsForge.java
@@ -1,15 +1,33 @@
 package net.mysticcreations.underthestars.forge;
 
 import dev.architectury.platform.forge.EventBuses;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import net.mysticcreations.underthestars.UnderTheStars;
+import net.mysticcreations.underthestars.worldgen.UTSRegion;
+import terrablender.api.Regions;
 
 @Mod(UnderTheStars.MODID)
 public final class UnderTheStarsForge {
+
+    public static IEventBus EVENT_BUS;
+
     public UnderTheStarsForge() {
-        EventBuses.registerModEventBus(UnderTheStars.MODID, FMLJavaModLoadingContext.get().getModEventBus());
+        EVENT_BUS = FMLJavaModLoadingContext.get().getModEventBus();
+        EventBuses.registerModEventBus(UnderTheStars.MODID, EVENT_BUS);
         UnderTheStars.init();
+
+        EVENT_BUS.addListener(this::commonSetup);
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            Regions.register(new UTSRegion(new ResourceLocation("under_the_stars:overworld_region"), 1));
+        });
     }
 }
